8a81408fca322987962678c891f05c63ca458329
Victor <spiritnightmare823@gmail.com>
2024-03-18 18:03:06 +0800

    change to new routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const pool = require("./database/database");
 const logger = require("./logger/logger");
 
-const customerRoutes = require('./routes/customers');
+const customerRoutes = require('./services/customer');
 const invociesRoutes = require('./routes/invoices');
 
 const app = express();
@@ -15,7 +15,6 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json()); 
 app.use(customerRoutes);
-app.use(invociesRoutes);
 
 // Using in an express application
 app.use((err, req, res, next) => {
